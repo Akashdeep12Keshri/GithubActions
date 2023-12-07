9403d714304dec45ce77220648a1d4213e69a666
Akashdeep12Keshri <36514101+Akashdeep12Keshri@users.noreply.github.com>
2023-12-07 10:47:46 +0530

    Create GithubAction-Script.sh

---
+++ GithubAction-Script.sh
@@ -0,0 +1,18 @@
+echo 1. what is in the directory 
+          ls -a
+          echo 
+          echo 2. Is git installed ?
+          git --version
+          echo
+          echo 3. Is java installed ?
+          java -version
+          echo 
+          echo 4. who is running the script
+          whoami
+          echo
+          echo 5. What environment variables are available ?
+          env
+          echo
+          
+          echo 7. How is the disc laid out ?
+          df
